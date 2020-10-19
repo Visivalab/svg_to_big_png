693a4239391e2cb17fbf6a7d80a1d014ef47c2fc
pingumen96 <sinhue.rossi@gmail.com>
2020-10-19 15:45:13 +0200

    Modifiche

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
+import math
 
 iterations = 35
 svgHeight = 3259
 svgWidth = 2244
+#svgHeight = 99
+#svgWidth = 91
+exportHeight = 1000
+filename = "layoutmappaHERA.svg"
 
 # probabilmente è possibile velocizzare il tutto usando parallelismo
 for x in range(0, iterations):
     for y in range(0, iterations):
-        command = """inkscape --export-area=%s:%s:%s:%s --export-filename=%s.png --export-height=40000
-                    layoutmappaHERA.svg""" %(x * (svgWidth / iterations), y * (svgHeight / iterations),
-                                             x * (svgWidth / iterations) + (svgWidth / iterations),
-                                             y * (svgHeight / iterations) + (svgHeight / iterations),
-                                             str(x) + "-" + str(y))
+        command = """inkscape --export-area=%s:%s:%s:%s --export-filename=%s.png --export-height=%s \
+                    %s""" %(x * math.ceil(svgWidth / iterations), y * math.ceil(svgHeight / iterations),
+                            x * math.ceil(svgWidth / iterations) + math.ceil(svgWidth / iterations),
+                            y * math.ceil(svgHeight / iterations) + math.ceil(svgHeight / iterations),
+                            str(x) + "-" + str(y),
+                            exportHeight,
+                            filename)
         os.system(command)
